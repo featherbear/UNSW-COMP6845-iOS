101507c9fb6752b16e0e89c3c22a570c5d699606
Andrew Wong <andrew.j.wong@outlook.com>
2023-07-24 02:12:52 +1000

    Teaching content

--- content/pages.ts
+++ content/pages.ts
@@ -1,9 +1,23 @@
-import Welcome from "./sections/01-welcome.svx";
-import Next from "./sections/02-next.svx"
+import resources from "./sections/00-resources.svx";
+import welcome from "./sections/01-welcome.svx";
+import secureBoot from "./sections/02-secure-boot-chain.svx";
+import iosFilesystem from "./sections/03-ios-filesystem.svx";
+import iosSandbox from "./sections/04-ios-sandbox.svx";
+import imageAcquisition from "./sections/05-image-acquisition.svx";
+import jailbreaking from "./sections/06-jailbreaking.svx";
+import passwordChallenges from "./sections/07-password-challenges.svx";
+import artifacts from "./sections/08-artifacts.svx";
 
 const pages = [
-  ["Welcome", Welcome],
-  ["Next", Next]
+  ["Resources", resources],
+  ["Introduction", welcome],
+  ["Secure Boot Chain", secureBoot],
+  ["Filesystem", iosFilesystem],
+  ["Sandbox", iosSandbox],
+  ["Image Acquisition", imageAcquisition],
+  ["Jailbreaking", jailbreaking],
+  ["Password Challenges", passwordChallenges],
+  ["Artifacts", artifacts],
 ] satisfies [string, any][];
 
 export default pages
