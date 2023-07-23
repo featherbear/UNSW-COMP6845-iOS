49514bca89333093ff5554f6f82a7bd9e18ee4f3
Andrew Wong <andrew.j.wong@outlook.com>
2023-07-24 04:02:08 +1000

    Lab

--- content/pages.ts
+++ content/pages.ts
@@ -7,6 +7,7 @@ import imageAcquisition from "./sections/05-image-acquisition.svx";
 import jailbreaking from "./sections/06-jailbreaking.svx";
 import passwordChallenges from "./sections/07-password-challenges.svx";
 import artifacts from "./sections/08-artifacts.svx";
+import lab from "./sections/09-lab.svx";
 
 const pages = [
   ["Resources", resources],
@@ -18,6 +19,7 @@ const pages = [
   ["Jailbreaking", jailbreaking],
   ["Password Challenges", passwordChallenges],
   ["Artifacts", artifacts],
+  ["Lab", lab],
 ] satisfies [string, any][];
 
 export default pages
